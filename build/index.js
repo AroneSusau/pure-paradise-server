d71d662dd3a26fb187863b5a253a9c2fd583a21c
Arone Susau <arone.s@live.com.au>
2020-02-11 11:18:14 +1100

    removing build from commits

--- build/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var b = 2;
-console.log(b);
-//# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,7 @@ import { Controller } from "./app/controller/Controller"
 let b = 2
 
 console.log(b)
+
+const y = new Controller()
+
+console.log(y)

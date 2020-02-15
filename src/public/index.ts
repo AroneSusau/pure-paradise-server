37f9484bd32d5487e27852a9d58fa4e84d431f40
Arone Susau <arone.s@live.com.au>
2020-02-15 18:42:07 +1100

    restructured continued

--- src/public/index.ts
+++ src/public/index.ts
@@ -1,7 +1,1 @@
-import { GameEngine } from "../private/app/model/gameEngine/GameEngine.js"
-
-const gameEngine = new GameEngine()
-
-console.dir(gameEngine)
-
-// testing
+console.log("testing testing 123")

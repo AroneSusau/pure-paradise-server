05961288bb79ecfe64a3489dd9f73a4bcb40784c
Arone Susau <arone.s@live.com.au>
2020-03-05 22:56:26 +1100

    updated build

--- src/public/js/SocketManager.js
+++ src/public/js/SocketManager.js
@@ -1,7 +1,7 @@
 module.exports = class SocketManager {
 
     constructor(io) {
-        this.socket = io("https://socket.ppv2.aronesusau.com:5050")
+        this.socket = io("https://socket.ppv2.aronesusau.com")
     }
 
     setTerminal(terminal) {

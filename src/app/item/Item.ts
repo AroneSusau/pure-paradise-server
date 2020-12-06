b459f1f4067cd546bd4aa575ae7cf460cc3ce5f2
Arone Susau <arone.s@live.com.au>
2020-12-07 10:03:06 +1100

    updating base item class

--- src/app/item/Item.ts
+++ src/app/item/Item.ts
@@ -1,31 +1,28 @@
 export abstract class Item {
 
-    constructor(id: number, name: string, description: string, price: number) {
+    protected _id: number
+    protected _name: string
+    protected _description: string
+    protected _price: number
+
+    protected constructor(id: number, name: string, description: string, price: number) {
         this._id = id
         this._name = name
         this._description = description
         this._price = price
     }
 
-    protected _id: number
-
     public get id(): number {
         return this._id
     }
-
-    protected _name: string
 
     public get name(): string {
         return this._name
     }
 
-    protected _description: string
-
     public get description(): string {
         return this._description
     }
-
-    protected _price: number
 
     public get price(): number {
         return this._price

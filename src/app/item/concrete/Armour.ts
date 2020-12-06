61618eb1ad069f79f0b84b080cd480355fea3032
Arone Susau <arone.s@live.com.au>
2020-12-07 10:01:11 +1100

    implementing item functionality

--- src/app/item/concrete/Armour.ts
+++ src/app/item/concrete/Armour.ts
@@ -1,10 +1,12 @@
 import {Usable} from '../interface/Usable'
 import {Player} from '../../character/concrete/Player'
 import {Item} from '../Item'
+import {ItemId} from "../../defaults/ItemId";
+import {items} from "./ItemsList";
 
 export class Armour extends Item implements Usable {
 
-    protected readonly defence: number
+    public readonly defence: number
 
     constructor(id: number, name: string, description: string, price: number, defence: number) {
         super(id, name, description, price)
@@ -12,9 +14,13 @@ export class Armour extends Item implements Usable {
     }
 
     public equip(player: Player): void {
+        player.meta.inventoryMeta.armour = this.id
+        player.stats.defence = this.defence
     }
 
     public unequip(player: Player): void {
+        player.meta.inventoryMeta.armour = ItemId.CLOTH_CLOTHING
+        player.stats.defence = (items.get(ItemId.CLOTH_CLOTHING) as Armour).defence
     }
 
 }
--- src/app/item/concrete/Weapon.ts
+++ src/app/item/concrete/Weapon.ts
@@ -1,10 +1,12 @@
 import {Usable} from '../interface/Usable'
 import {Player} from '../../character/concrete/Player'
 import {Item} from '../Item'
+import {ItemId} from "../../defaults/ItemId";
+import {items} from "./ItemsList";
 
 export class Weapon extends Item implements Usable {
 
-    protected readonly damage: number
+    public readonly damage: number
 
     constructor(id: number, name: string, description: string, price: number, damage: number) {
         super(id, name, description, price)
@@ -12,9 +14,13 @@ export class Weapon extends Item implements Usable {
     }
 
     public equip(player: Player): void {
+        player.meta.inventoryMeta.weapon = this.id
+        player.stats.damage = this.damage
     }
 
     public unequip(player: Player): void {
+        player.meta.inventoryMeta.weapon = ItemId.FISTS
+        player.stats.damage = (items.get(ItemId.FISTS) as Weapon).damage
     }
 
 }
